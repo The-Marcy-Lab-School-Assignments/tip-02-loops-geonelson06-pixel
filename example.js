2a15d367334cb6007f64150f09c6434e8adef06b
Gabriel <geonelson06@gmail.com>
2025-10-01 11:10:53 -0400

    Completed loops challenge

--- example.js
+++ example.js
@@ -2,6 +2,7 @@ function staircaseSum(n) {
   let total = 0; // Initialize a variable to keep track of the sum
 
   for (let i = 1; i <= n; i++) {
+    console.log(i);
     // Loop from 1 up to the given number
     total += i; // Add the current number to the total
   }
@@ -9,4 +10,4 @@ function staircaseSum(n) {
   return total;
 }
 
-console.log(staircaseSum(5)); // Call the function with 5; output: 15
+console.log(staircaseSum(9)); // Call the function with 5; output: 15
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // Write a function that prints the first n square numbers and also calculates the sum of these numbers.
 const printSquareNumbers = (n) => {
-  //write code here
+  let total = 0
+  
 
+  for (let i = 1; i < n; i++)
+    let sqr = i * i;
+    console.log(sqr);
+    total += sqr
 }
 printSquareNumbers(5);
 //will print: 1, 4, 9, 16, 25
